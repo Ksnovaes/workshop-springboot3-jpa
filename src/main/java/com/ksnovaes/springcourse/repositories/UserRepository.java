2429903ea6325843103d0ec89e88142541ae0bc0
Kauã Novaes <kauanovais84@gmail.com>
2024-01-16 15:37:07 -0300

    feat: implement service layer and component registration

--- src/main/java/com/ksnovaes/springcourse/repositories/UserRepository.java
+++ src/main/java/com/ksnovaes/springcourse/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.ksnovaes.springcourse.repositories;
 
 import com.ksnovaes.springcourse.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/ksnovaes/springcourse/services/UserService.java
@@ -0,0 +1,24 @@
+package com.ksnovaes.springcourse.services;
+
+import com.ksnovaes.springcourse.entities.User;
+import com.ksnovaes.springcourse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService{
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
