9fecf96ed44f04b1e995041f4cad3056ae49b4b6
Kauã Novaes <kauanovais84@gmail.com>
2024-01-16 16:28:01 -0300

    feat: add orderstatus enum

--- src/main/java/com/ksnovaes/springcourse/entities/Order.java
+++ src/main/java/com/ksnovaes/springcourse/entities/Order.java
@@ -1,6 +1,7 @@
 package com.ksnovaes.springcourse.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ksnovaes.springcourse.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -22,10 +24,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -42,6 +45,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
