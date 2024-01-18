4167ca1acde7fbc18c7b476cc475b510a1c83c9e
Kauã Novaes <kauanovais84@gmail.com>
2024-01-18 17:00:50 -0300

    feat(exception): add exception handling update

--- src/main/java/com/ksnovaes/springcourse/services/UserService.java
+++ src/main/java/com/ksnovaes/springcourse/services/UserService.java
@@ -4,6 +4,7 @@ import com.ksnovaes.springcourse.entities.User;
 import com.ksnovaes.springcourse.repositories.UserRepository;
 import com.ksnovaes.springcourse.services.exceptions.DatabaseException;
 import com.ksnovaes.springcourse.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService{
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
